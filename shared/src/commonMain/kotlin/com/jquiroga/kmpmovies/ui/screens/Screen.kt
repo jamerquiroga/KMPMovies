1be0ea30528425ad1e9c870fe0316b00245ce1fd
Jamer Quiroga <jamerquiroga@gmail.com>
2026-05-25 13:52:37 -0500

    add support for dark mode

--- shared/src/commonMain/kotlin/com/jquiroga/kmpmovies/ui/screens/Screen.kt
+++ shared/src/commonMain/kotlin/com/jquiroga/kmpmovies/ui/screens/Screen.kt
@@ -1,14 +1,19 @@
 package com.jquiroga.kmpmovies.ui.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Screen(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    MaterialTheme {
+    val isDarkMode = isSystemInDarkTheme()
+    val colorScheme = if (isDarkMode) darkColorScheme() else lightColorScheme()
+    MaterialTheme(colorScheme = colorScheme) {
         Surface(
             modifier = modifier.fillMaxSize(),
             content = content
