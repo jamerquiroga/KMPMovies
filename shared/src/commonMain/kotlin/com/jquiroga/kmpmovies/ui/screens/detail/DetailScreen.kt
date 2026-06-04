05ef928770d423e9f8878a3fdd9bee60a338f156
Jamer Quiroga <jamerquiroga@gmail.com>
2026-06-03 19:59:16 -0500

    detail screen enhancements

--- shared/src/commonMain/kotlin/com/jquiroga/kmpmovies/ui/screens/detail/DetailScreen.kt
+++ shared/src/commonMain/kotlin/com/jquiroga/kmpmovies/ui/screens/detail/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.jquiroga.kmpmovies.ui.screens.detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,60 +15,145 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import com.jquiroga.kmpmovies.data.MovieDetail
 import com.jquiroga.kmpmovies.ui.commom.LoadingIndicator
 import com.jquiroga.kmpmovies.ui.screens.Screen
 import kmpmovies.shared.generated.resources.Res
 import kmpmovies.shared.generated.resources.back
+import kmpmovies.shared.generated.resources.original_language
+import kmpmovies.shared.generated.resources.original_title
+import kmpmovies.shared.generated.resources.popularity
+import kmpmovies.shared.generated.resources.release_date
+import kmpmovies.shared.generated.resources.vote_average
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun DetailScreen(viewModel: DetailViewModel, onBack: () -> Unit) {
     val state = viewModel.state
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Screen {
         Scaffold(
             topBar = {
-                TopAppBar(
-                    title = { Text(state.movieDetail.title) },
-                    navigationIcon = {
-                        IconButton(onClick = onBack) {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = stringResource(Res.string.back)
-                            )
-                        }
-                    }
+                DetailTopBar(
+                    title = state.movieDetail.title,
+                    onBack = onBack,
+                    scrollBehavior = scrollBehavior
                 )
             }
         ) { paddingValues ->
 
-            LoadingIndicator(enable = state.loading)
+            LoadingIndicator(
+                enable = state.loading,
+                modifier = Modifier.padding(paddingValues)
+            )
 
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                AsyncImage(
-                    model = state.movieDetail.backdropPath,
-                    contentDescription = state.movieDetail.title,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(16f / 9f)
-                )
-                Text(
-                    text = state.movieDetail.overview,
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineMedium
-                )
+            MovieDetail(
+                modifier = Modifier.padding(paddingValues),
+                movieDetail = state.movieDetail
+            )
+        }
+    }
+}
 
+@Composable
+private fun DetailTopBar(
+    title: String,
+    onBack: () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(Res.string.back)
+                )
             }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+@Composable
+private fun MovieDetail(
+    modifier: Modifier = Modifier,
+    movieDetail: MovieDetail
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        AsyncImage(
+            model = movieDetail.backdropPath,
+            contentDescription = movieDetail.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(16f / 9f)
+        )
+        Text(
+            text = movieDetail.overview,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = buildAnnotatedString {
+                property(
+                    name = stringResource(Res.string.original_language),
+                    value = movieDetail.originalLanguage
+                )
+                property(
+                    name = stringResource(Res.string.original_title),
+                    value = movieDetail.originalTitle
+                )
+                property(
+                    name = stringResource(Res.string.release_date),
+                    value = movieDetail.releaseDate
+                )
+                property(
+                    name = stringResource(Res.string.popularity),
+                    value = movieDetail.popularity.toString()
+                )
+                property(
+                    name = stringResource(Res.string.vote_average),
+                    value = movieDetail.voteAverage.toString()
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                .padding(16.dp)
+        )
+    }
+}
+
+private fun AnnotatedString.Builder.property(
+    name: String,
+    value: String,
+    end: Boolean = false
+) {
+    withStyle(ParagraphStyle(lineHeight = 18.sp)) {
+        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+            append("$name : ")
+        }
+        append(value)
+        if (!end) {
+            append("\n")
         }
     }
 }
