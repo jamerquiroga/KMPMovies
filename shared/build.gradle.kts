74a825dc698930b7c67b9d4e594191c12d217b7c
Jamer Quiroga <jamerquiroga@gmail.com>
2026-06-08 13:22:57 -0500

    remove unnecessary library imports

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -43,11 +43,9 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.ktor.client.okhttp)
-            implementation(libs.room.runtime)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
-            implementation(libs.room.runtime)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
